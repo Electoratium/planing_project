80d778c1345052d4c2d84e6f6ef41da9be01c439
Electoratium <electoratium@gmail.com>
2019-04-16 20:23:01 +0300

    add test editor on home page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,27 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 // import {connect} from 'react-redux';
 // import {bindActionCreators} from 'redux';
 
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
 
 class Home extends Component {
-  // showCars () {
-  //     return this.props.cars.map( car => {
-  //         return (
-  //             <li  key={car.id}>{car.name}</li>
-  //         );
-  //     });
-  // }
+	constructor(props) {
+		super(props);
 
-  render() {
-    return (
-      <div>
-        <ul>
-          {/* {this.showCars()} */}
-        </ul>
-        <p>Home page</p>
-      </div>
-    );
-  }
+
+
+		const editorSettigns = {
+				holderId: 'editor',
+				autofocus: true,
+				tools: {
+					// header can be changed on heading etc
+				    header: Header,
+				    list: List,
+				},
+
+				onReady() {
+					console.log('I am ready. Sorry, but i\m async editor :( ');
+				},
+				onChange() {
+					console.log('pss! something change in me!');
+				}
+			},
+			editor = new EditorJS(editorSettigns);
+
+		editor.isReady
+			.then( () => {
+				console.log('i am also a promise!');
+			})
+			.catch( (err) => {
+				console.log(err);
+			});
+
+		this.state = {
+			editor
+		};
+	}
+	saveData = () => {
+		const {editor} = this.state;
+
+		editor.save()
+			.then((outputData) => {
+	            console.log('Article data: ', outputData)
+			})
+			.catch((error) => {
+	            console.log('Saving failed: ', error)
+			});
+	};
+	showTextEditor = () => (
+		<Fragment>
+			<div id="editor" style={{border: '1px dashed gray'}}></div>
+			<button onClick={this.saveData}>Save editor</button>
+		</Fragment>
+
+	);
+
+	render() {
+		return (
+			<div>
+				<ul>
+					{this.showTextEditor()}
+				</ul>
+				<p>Home page</p>
+			</div>
+		);
+	}
 }
 
 //
